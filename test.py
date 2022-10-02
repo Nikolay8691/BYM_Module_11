5097a1810236a905de0d24d5806506a2250f4f86
Nikolay V <nv-mail@mail.ru>
2022-10-02 16:13:40 +0300

    doc strings are provided for all tests functions

--- test.py
+++ test.py
@@ -8,11 +8,13 @@ class Tests(unittest.TestCase):
 	"""testing 'merge' sorting function"""
 
 	def test_1(self):
+		"""testing 'merge' sorting function compare to standart function 'sorted'"""
 		a = merge_sort.a
 		b = merge_sort.b
 		self.assertEqual(b, sorted(a))
 
 	def test_2(self):
+		"""testing 'merge' sorting function whether it raises number with every step"""
 		n = len(merge_sort.a)
 		b = merge_sort.b
 		j = 1
@@ -25,6 +27,7 @@ class Tests(unittest.TestCase):
 
 
 	def test_3(self):
+		"""testing 'merge' sorting function whether [0] element is min"""
 		a = merge_sort.a
 		b = merge_sort.b
 		amin = a[0]
@@ -32,10 +35,11 @@ class Tests(unittest.TestCase):
 		self.assertTrue(amin >= bmin)		
 
 	def test_4(self):
+		"""testing 'merge' sorting function whether [-1] element is max"""
 		a = merge_sort.a
 		b = merge_sort.b
-		amax = a[len(a)-1]
-		bmax = b[len(b)-1]
+		amax = a[-1]
+		bmax = b[-1]
 		self.assertFalse(amax > bmax)
 
 if __name__ == "__main__":
